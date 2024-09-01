02079e2692d652bb4733aeda7ddd878e9305f9ef
nataliafatkina <fatkina.natali@gmail.com>
2024-09-01 13:06:11 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,12 +1,16 @@
 # input data
-first = int(input("Введите первое число: "))
-second = int(input("Введите второе число: "))
-third = int(input("Введите третье число: "))
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
 
 # program
 
 if first == second == third:
     print(3)
-elif first == second != third or first != second == third or first == third != second:
+elif first == second != third:
+    print(2)
+elif first != second == third:
+    print(2)
+elif first == third != second:
     print(2)
 else: print(0)
